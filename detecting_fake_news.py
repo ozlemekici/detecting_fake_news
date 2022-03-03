53690f7a6581051c89861ca0bca8335c522a0e50
Ozlem E <ozlemekici9774@gmail.com>
2022-03-03 03:05:28 +0300

    minor fixes

--- detecting_fake_news.py
+++ detecting_fake_news.py
@@ -16,34 +16,34 @@ df.shape
 df.head()
 
 # %%
-# DataFlair - Get the labels
+# Get the labels
 labels = df.label
 labels.head()
 
 # %%
-# DataFlair - Split the dataset
+# Split the dataset
 x_train, x_test, y_train, y_test = train_test_split(
     df['text'], labels, test_size=0.2, random_state=7)
 
 # %%
-# DataFlair - Initialize a TfidfVectorizer
+# Initialize a TfidfVectorizer
 tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
-# DataFlair - Fit and transform train set, transform test set
+# Fit and transform train set, transform test set
 tfidf_train = tfidf_vectorizer.fit_transform(x_train)
 tfidf_test = tfidf_vectorizer.transform(x_test)
 
 # %%
-# DataFlair - Initialize a PassiveAggressiveClassifier
+# Initialize a PassiveAggressiveClassifier
 pac = PassiveAggressiveClassifier(max_iter=50)
 pac.fit(tfidf_train, y_train)
 
-# DataFlair - Predict on the test set and calculate accuracy
+# Predict on the test set and calculate accuracy
 y_pred = pac.predict(tfidf_test)
 score = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {round(score*100,2)}%')
 
 # %%
-# DataFlair - Build confusion matrix
+# Build confusion matrix
 confusion_matrix(y_test, y_pred, labels=['FAKE', 'REAL'])
 
 # %%
